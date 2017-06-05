3bd2730fb60d3966f694f2ae5290f3f185157675
anmolduainter <anmolduainter@gmail.com>
2017-06-05 13:50:45 +0530

    .
    .

--- app/src/main/java/com/example/joginderpal/safero/Main/firstActivity.java
+++ app/src/main/java/com/example/joginderpal/safero/Main/firstActivity.java
@@ -5,11 +5,15 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TimePicker;
 
 import com.example.joginderpal.safero.R;
 import com.example.joginderpal.safero.Service.powerButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by joginderpal on 05-06-2017.
@@ -17,8 +21,10 @@ import com.example.joginderpal.safero.Service.powerButton;
 
 public class firstActivity extends AppCompatActivity {
 
+    @BindView(R.id.startservice)
     Switch serviceSwitch;
 
+    @BindView(R.id.timePick)
     TimePicker timePicker;
 
     @Override
@@ -26,11 +32,36 @@ public class firstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firstactivity);
 
-        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        powerButton btn = new powerButton (this);
-        registerReceiver(btn, filter);
+        ButterKnife.bind(this);
 
 
+        serviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+
+
+                }
+                else{
+
+
+
+                }
+
+            }
+        });
+
+        if (serviceSwitch.isChecked()){
+            IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            powerButton btn = new powerButton (this);
+            registerReceiver(btn, filter);
+        }
+        else{
+
+
+
+        }
     }
 }
